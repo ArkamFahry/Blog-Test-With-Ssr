b68164b06f59b0206cd7030d61f8619e22420737
ArkamFahry <Arkam.fahry@gmail.com>
2021-11-30 19:20:12 -0800

    bug fix

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -35,7 +35,10 @@ export async function getStaticProps({ params }) {
   const posts = await getCategoryPost(params.slug);
 
   return {
-    props: { posts },
+    props: { 
+      posts: posts
+    },
+    revalidate: 10
   };
 }
 
