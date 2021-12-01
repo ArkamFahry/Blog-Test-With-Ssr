e003a2feb01272031b0bb545b4d21db05f6d464a
ArkamFahry <Arkam.fahry@gmail.com>
2021-12-01 00:58:27 -0800

    bug fix

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -31,22 +31,21 @@ const CategoryPost = ({ posts }) => {
 export default CategoryPost;
 
 // Fetch data at build time
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const posts = await getCategoryPost(params.slug);
 
   return {
     props: { posts },
-    revalidate: 1
   };
 }
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-  const categories = await getCategories();
-  return {
-    paths: categories.map(({ slug }) => ({ params: { slug } })),
-    fallback: 'blocking'
-  };
-}
+// export async function getStaticPaths() {
+//   const categories = await getCategories();
+//   return {
+//     paths: categories.map(({ slug }) => ({ params: { slug } })),
+//     fallback: 'blocking'
+//   };
+// }
 
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -37,22 +37,21 @@ const PostDetails = ({ post }) => {
 export default PostDetails;
 
 // Fetch data at build time
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const data = await getPostDetails(params.slug);
   return {
     props: {
       post: data,
     },
-    revalidate: 1
   };
 }
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-  const posts = await getPosts();
-  return {
-    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: 'blocking'
-  };
-}
+// export async function getStaticPaths() {
+//   const posts = await getPosts();
+//   return {
+//     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+//     fallback: 'blocking'
+//   };
+// }
